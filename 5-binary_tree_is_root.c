2477d718f8dadfdb9222beeb2a92bbb6db328b9f
Initech <inimfonebong001@gmail.com>
2023-08-29 03:34:09 -0700

    fixed betty style

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_root - checks if a given node is a root
  * @node: is a pointer to the node to check
- * 
+ *
  * Return: 1 if node is a root, otherwise 0
  * If node is NULL, return 0
  */
@@ -14,6 +14,6 @@ int binary_tree_is_root(const binary_tree_t *node)
 		return (0);
 
 	if (node->parent)
-		return 0;
+		return (0);
 	return (1);
 }
